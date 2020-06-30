78ed5100dd6ee6a5c4a2ab687c289f4c8f4af10f
Leslie Samantha Garcia <leslie.samantha.garcia@gmail.com>
2020-06-30 14:13:46 -0500

    completed assignment

---
+++ js/while.js
@@ -0,0 +1,24 @@
+let num = 1;
+
+while (num <= 65536) {
+    num *= 2;
+    console.log(num);
+}
+
+
+
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+
+do {
+    let cones = Math.floor(Math.random() * 5) + 1;
+    if (cones <= allCones) {
+        allCones = allCones - cones;
+        console.log(cones + " cones sold...")
+    } else {
+        console.log("Cannot sell you " + cones + " cones I only have " + allCones + "...");
+    }
+
+} while (allCones > 0);
